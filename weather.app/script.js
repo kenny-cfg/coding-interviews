ec21c8c590cb3b108893900902c1fe30f3df51a1
Kenny Hung (he) <kenny.cfg@outlook.com>
2026-09-15 20:33:13 +0100

    Fetch from an API (not the one in the slides

--- weather.app/script.js
+++ weather.app/script.js
@@ -1,6 +1,19 @@
 const searchButton = document.getElementById('search');
 const cityNameInput = document.getElementById('city-name');
+
 searchButton.addEventListener('click', () => {
   const text = cityNameInput.value;
-  console.log('CLICK!', text);
+  populateSearch(text);
 })
+
+const populateSearch = async (cityName) => {
+  const response = await fetch("https://icanhazdadjoke.com", {
+    method: "GET",
+    headers: {
+      "Accept": "application/json",
+    },
+  });
+  const body = await response.json()
+  const joke = body.joke;
+  console.log(joke)
+}
